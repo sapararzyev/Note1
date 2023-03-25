806800b89980969e2f47e57cccbc73d0c789126d
sapararzyev <arzyev680@gmail.com>
2023-03-25 16:18:50 +0600

    some changens

--- app/src/main/java/com/example/note1/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/example/note1/presentation/activity/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.noteapp.presentation.activity
 
+import android.os.Build.VERSION_CODES.R
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.noteapp.R
@@ -10,5 +11,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val a =2
     }
 }
